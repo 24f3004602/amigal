a4d119d3482fc4ce8006e327c6bf1863778c5a5e
AMRINDER SINGH <24f3004602@ds.study.iitm.ac.in>
2026-08-03 21:47:26 +0530

    Update auth.module.ts

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { GoogleStrategy } from './strategies/google.strategy';
-import { FacebookStrategy } from './strategies/facebook.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    PrismaModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategy, FacebookStrategy],
+  providers: [AuthService, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
