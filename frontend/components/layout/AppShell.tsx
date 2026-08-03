3df973d3e316dbf4afa3c05c4d5deed33fbee8b8
AMRINDER SINGH <24f3004602@ds.study.iitm.ac.in>
2026-08-03 22:24:54 +0530

    Create AppShell.tsx

---
+++ frontend/components/layout/AppShell.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from './Navbar';
+
+export function AppShell({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen flex-col bg-background">
+      <Navbar />
+      <main className="flex-1 overflow-y-auto scrollbar-thin">
+        <div className="mx-auto max-w-7xl px-4 py-6 lg:px-8 lg:py-8">
+          {children}
+        </div>
+      </main>
+    </div>
+  );
+}
